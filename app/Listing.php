7152113e07e93c7e59c06c196cd88344ab4522f2
Ryan Crawford <ryanccrawford@live.com>
2019-10-14 07:46:30 -0400

    add some ini code

--- app/Listing.php
+++ app/Listing.php
@@ -2,9 +2,33 @@
 
 namespace App;
 
+use DTS\eBaySDK\Credentials\CredentialsProvider;
+use DTS\eBaySDK\inventory\Services;
+use DTS\eBaySDK\Inventory\Services\InventoryService;
 use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
-    //
+
+    public $provider;
+    public $inventoryService;
+
+
+    public function constuctor()
+    {
+
+        $this->provider = CredentialsProvider::ini();
+        $this->provider = CredentialsProvider::memoize($this->provider);
+        $sdk = new DTS\eBaySDK\Sdk(
+            [
+                'globalId'   => 'EBAY-US',
+            ]
+        );
+        $this->inventoryService = $sdk->createInventory(
+            [
+                'credentials' => $this->provider,
+            ]
+        );
+        $request = new DTS\eBaySDK\Inventory\GetInventoryItemRestRequest();
+    }
 }
