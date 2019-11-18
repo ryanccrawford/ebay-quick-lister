f610a3d3dfe604a8ff0d8516332246a809677ef7
Ryan Crawford <ryanccrawford@live.com>
2019-11-18 02:16:43 -0500

    12313

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Jenssegers\Mongodb\MongodbServiceProvider::class,
+
 
 
         /*
@@ -179,7 +179,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\CkServiceProvider::class,
 
@@ -233,7 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Moloquent' => Jenssegers\Mongodb\Eloquent\Model::class,
+
 
     ],
 
