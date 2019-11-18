01e4aab1506be0b70c0aab9bdb224932d9238d08
Ryan Crawford <ryanccrawford@live.com>
2019-11-18 05:55:41 -0500

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,11 @@
     </div>
 
                  @yield('last')
-
+                <script
+                        src="https://code.jquery.com/jquery-3.4.1.min.js"
+                        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+                        crossorigin="anonymous">
+                </script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
                 <script src="vendor/ckeditor/ckeditor.js"></script>
