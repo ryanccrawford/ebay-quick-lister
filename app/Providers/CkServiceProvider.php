4174089af03cac05bf92e77535b70ac0b64edb60
Ryan Crawford <ryanccrawford@live.com>
2019-11-18 05:49:43 -0500

    ckeditor

--- app/Providers/CkServiceProvider.php
+++ app/Providers/CkServiceProvider.php
@@ -8,19 +8,18 @@ class CkServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->publishes(
             [
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/ckeditor.js' => public_path('vendor/ckeditor/ckeditor/ckeditor.js'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/config.js' => public_path('vendor/ckeditor/ckeditor/config.js'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/styles.js' => public_path('vendor/ckeditor/ckeditor/styles.js'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/contents.css' => public_path('vendor/ckeditor/ckeditor/contents.css'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/adapters' => public_path('vendor/ckeditor/ckeditor/adapters'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/lang' => public_path('vendor/ckeditor/ckeditor/lang'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/skins' => public_path('vendor/ckeditor/ckeditor/skins'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/plugins' => public_path('vendor/ckeditor/ckeditor/plugins'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/ckeditor.js' => public_path('vendor/ckeditor/ckeditor.js'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/config.js' => public_path('vendor/ckeditor/config.js'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/styles.js' => public_path('vendor/ckeditor/styles.js'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/contents.css' => public_path('vendor/ckeditor/contents.css'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/adapters' => public_path('vendor/ckeditor/adapters'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/lang' => public_path('vendor/ckeditor/lang'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/skins' => public_path('vendor/ckeditor/skins'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/plugins' => public_path('vendor/ckeditor/plugins'),
             ],
             'ckeditor'
-        ); 
+        );
     }
     public function register()
-    {
-    }
+    { }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,11 +34,11 @@
     </div>
 
                  @yield('last')
-                
+
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
-                <script src="{{ asset('vendor/ckeditor/ckeditor/ckeditor.js') }}"></script>
-                <script src="{{ asset('vendor/ckeditor/ckeditor/adapters/jquery.js') }}"></script>
+                <script src="vendor/ckeditor/ckeditor.js"></script>
+                <script src="vendor/ckeditor/adapters/jquery.js"></script>
     @stack('end')
 
 </body>
