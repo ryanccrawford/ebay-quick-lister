3b5980abedd319400e5cceb051706f4d7ac71981
Ryan Crawford <ryanccrawford@live.com>
2019-11-18 06:36:36 -0500

    update

--- app/Providers/CkServiceProvider.php
+++ app/Providers/CkServiceProvider.php
@@ -8,14 +8,14 @@ class CkServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->publishes(
             [
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/ckeditor.js' => public_path('vendor/ckeditor/ckeditor.js'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/config.js' => public_path('vendor/ckeditor/config.js'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/styles.js' => public_path('vendor/ckeditor/styles.js'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/contents.css' => public_path('vendor/ckeditor/contents.css'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/adapters' => public_path('vendor/ckeditor/adapters'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/lang' => public_path('vendor/ckeditor/lang'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/skins' => public_path('vendor/ckeditor/skins'),
-                __DIR__ . '/../../vendor/ckeditor/ckeditor/plugins' => public_path('vendor/ckeditor/plugins'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/ckeditor.js' => public_path('js/ckeditor/ckeditor.js'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/config.js' => public_path('js/ckeditor/config.js'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/styles.js' => public_path('js/ckeditor/styles.js'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/contents.css' => public_path('js/ckeditor/contents.css'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/adapters' => public_path('js/ckeditor/adapters'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/lang' => public_path('js/ckeditor/lang'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/skins' => public_path('js/ckeditor/skins'),
+                __DIR__ . '/../../vendor/ckeditor/ckeditor/plugins' => public_path('js/ckeditor/plugins'),
             ],
             'ckeditor'
         );
